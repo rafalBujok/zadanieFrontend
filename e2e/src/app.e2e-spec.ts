e5e8c521251e59bc118cb1127ee1083d94de25ee
Rafał Bujok <rafal.bujok@gmail.com>
2021-03-24 23:23:02 +0100

    fix bugs

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('zadanieTestoweFrontend app is running!');
+    expect(page.getTitleText()).toEqual('unslpashApp app is running!');
   });
 
   afterEach(async () => {
